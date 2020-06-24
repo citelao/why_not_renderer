72d88c507999442df24a3994ed39f3d637417492
Ben Stolovitz <ben@stolovitz.com>
2020-06-24 17:43:35 -0400

    render a nice square

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,8 +3,8 @@ console.log("Loading renderer!");
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
-const WIDTH = 40;
-const HEIGHT = 60;
+const WIDTH = 512;
+const HEIGHT = 512;
 const imageData = ctx.createImageData(WIDTH, HEIGHT);
 const data = imageData.data;
 
@@ -32,6 +32,19 @@ function setPixel(data: Uint8ClampedArray, pos: Pos, color: Color) {
     data[offset + 3] = color.a || 255;
 }
 
+function repeat(times: number, fn: (index: number) => void) {
+    for (let i = 0; i < times; i++) {
+        fn(i);
+    }
+}
+
+repeat(WIDTH, (x) => {
+    repeat(HEIGHT, (y) => {
+        setPixel(data, { x, y }, { r: 255, g: 0, b: 100});
+    })
+})
+
 setPixel(data, { x: 10, y: 4}, { r: 255, g: 150, b: 100});
 setPixel(data, { x: 15, y: 24}, { r: 255, g: 150, b: 100});
+
 ctx.putImageData(imageData, 0, 0);
