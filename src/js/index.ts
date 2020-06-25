52b3365808c6633e01bc702177b284492b48102a
Ben <ben@stolovitz.com>
2020-06-25 17:39:44 -0400

    multiple light rays

--- src/js/index.ts
+++ src/js/index.ts
@@ -215,20 +215,29 @@ function cast(scene: IScene, ray: Ray3D, iteration = 0): Color {
     }
 
     // Cast light rays
+    const LIGHT_RAY_COUNT = 3;
+    const LIGHT_SPREAD_AMOUNT = 0.3;
     const light_intersections = scene.lights.map((light) => {
-        const lightRay = Ray3D.Create({
-            pt: lastCollision.collision.point,
-            dir: light.center.minus(lastCollision.collision.point).normalized()
-        })
+        const successful_light_rays: boolean[] = [];
+        repeat(LIGHT_RAY_COUNT, () => {
+            const lightRay = Ray3D.Create({
+                pt: lastCollision.collision.point,
+                dir: perturb(light.center.minus(lastCollision.collision.point).normalized(), LIGHT_SPREAD_AMOUNT)
+            });
+    
+            const lightCollision = collideRay(scene, lightRay);
+            const reached_light = lightCollision.length === 0;
+            successful_light_rays.push(reached_light);
+        });
 
-        const lightCollisions = collideRay(scene, lightRay);
+        const count = successful_light_rays.filter((v) => v === true).length;
 
         return {
             light: light,
-            collisions: lightCollisions
+            reachedPercent: count / successful_light_rays.length
         };
     }).filter((v) => {
-        return v.collisions.length === 0;
+        return v.reachedPercent !== 0;
     });
 
     const light_color = light_intersections.reduce((accum, current) => {
@@ -237,7 +246,7 @@ function cast(scene: IScene, ray: Ray3D, iteration = 0): Color {
         //
         // TODO: colored lights
         const light_distance = current.light.center.minus(ray.pt).magnitude();
-        const increase = current.light.intensity / ((light_distance / 500) ** 2);
+        const increase = current.light.intensity / ((light_distance / 500) ** 2) * current.reachedPercent;
 
         return {
             r: accum.r + increase,
@@ -258,15 +267,8 @@ function cast(scene: IScene, ray: Ray3D, iteration = 0): Color {
         dir: bounceNorm
     });
 
-    // return normalToColor(bounceNorm);
-    // return {
-    //     r: bounceNorm.x * COLOR_MAX,
-    //     g: bounceNorm.y * COLOR_MAX,
-    //     b: bounceNorm.z * COLOR_MAX,
-    // }
-
     const BOUNCE_COUNT = 3;
-    const SPREAD_AMOUNT = 0.1;
+    const SPREAD_AMOUNT = 0.3;
     const bounces: Color[] = [];
     repeat(BOUNCE_COUNT, (i) => {
         const perturbedRay = Ray3D.Create({
@@ -294,7 +296,7 @@ function cast(scene: IScene, ray: Ray3D, iteration = 0): Color {
     //     newRay,
     //     iteration + 1);
 
-    const mix = 0.5;
+    const mix = 0.3;
     const imix = 1 - mix;
     return {
         r: light_color.r * imix + bounce.r * mix,
